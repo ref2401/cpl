9a56cf89e43bbfa25ef1923d95c34073b1d3e7fc
ref2401 <refactor24@gmail.com>
2017-04-07 17:42:07 +0300

    concurrent_queue uses ring_buffer

--- inlcude/cpl/concurrent_queue.h
+++ inlcude/cpl/concurrent_queue.h
@@ -16,13 +16,11 @@ template<typename T>
 class concurrent_queue final {
 public:
 
-	explicit concurrent_queue(size_t capacity_limit);
+	explicit concurrent_queue(size_t size_limit);
 
 	concurrent_queue(const concurrent_queue&) = delete;
 	concurrent_queue& operator=(const concurrent_queue&) = delete;
 
-
-	//void clear();
 
 	bool empty() const;
 
@@ -40,45 +38,29 @@ public:
 
 private:
 
-	bool empty_impl() const noexcept { return (pop_index == push_index); }
-
-	size_t size_impl() const noexcept { return (push_index - pop_index); };
-
-	void pop_impl(T& out_v) noexcept
-	{
-		assert(pop_index <= push_index);
-
-		out_v = std::move(buffer[pop_index]);
-		++pop_index;
-
-		if (pop_index == push_index) {
-			push_index = 0;
-			pop_index = 0;
-		}
-	}
-
-
-	ring_buffer<T> _queue;
+	ring_buffer<T> queue;
 	mutable std::mutex mutex;
 	std::condition_variable not_empty_condition;
-	
 };
 
 
 template<typename T>
-concurrent_queue<T>::concurrent_queue(size_t capacity_limit)
-	: buffer(capacity_limit),
-	push_index(0),
-	pop_index(0)
-{
-	assert(capacity_limit > 0);
-}
+concurrent_queue<T>::concurrent_queue(size_t size_limit)
+	: queue(size_limit)
+{}
 
 template<typename T>
 bool concurrent_queue<T>::empty() const
 {
 	std::lock_guard<std::mutex> lock(mutex);
-	return empty_impl();
+	return queue.empty();
+}
+
+template<typename T>
+size_t concurrent_queue<T>::size() const
+{
+	std::lock_guard<std::mutex> lock(mutex);
+	return queue.size();
 }
 
 template<typename T>
@@ -89,35 +71,20 @@ void concurrent_queue<T>::push(U&& v)
 
 	{
 		std::lock_guard<std::mutex> lock(mutex);
-		//assert(push_index < capacity_limit);
-
-		//if (push_index >= capacity_limit) {
-		//	int k = 0;
-		//	++k;
-		//}
-
-		buffer[push_index] = std::forward<U>(v);
-		++push_index;
+		bool res = queue.try_push(std::forward<U>(v));
+		assert(res);
 	}
 
 	not_empty_condition.notify_one();
 }
 
 template<typename T>
-size_t concurrent_queue<T>::size() const
-{
-	std::lock_guard<std::mutex> lock(mutex);
-	return size_impl();
-}
-
-template<typename T>
 bool concurrent_queue<T>::try_pop(T& out_v)
 {
 	std::lock_guard<std::mutex> lock(mutex);
-	if (empty_impl()) return false;
+	if (queue.empty()) return false;
 
-	pop_impl(out_v);
-	return true;
+	return queue.try_pop(out_v);
 }
 
 template<typename T>
@@ -125,12 +92,11 @@ bool concurrent_queue<T>::wait_pop(T& out_v, const std::atomic_bool& wait_allowe
 {
 	// wait until the queue is empty and waiting is allowed.
 	std::unique_lock<std::mutex> lock(mutex);
-	not_empty_condition.wait(lock, [this, &wait_allowed] { return !(empty_impl() && wait_allowed); });
+	not_empty_condition.wait(lock, [this, &wait_allowed] { return !(queue.empty() && wait_allowed); });
 
 	if (!wait_allowed) return false;
 
-	pop_impl(out_v);
-	return true;
+	return queue.try_pop(out_v);
 }
 
 } // namespace cpl
--- src/unittest/concurrent_queue_unittest.cpp
+++ src/unittest/concurrent_queue_unittest.cpp
@@ -17,121 +17,121 @@ namespace unittest {
 TEST_CLASS(concurrent_queue_concurrent_queue) {
 public:
 
-	//TEST_METHOD(ctors)
-	//{
-	//	concurrent_queue<int> queue(4);
-	//	Assert::IsTrue(queue.empty());
-	//	Assert::AreEqual<size_t>(0, queue.size());
-	//}
+	TEST_METHOD(ctors)
+	{
+		concurrent_queue<int> queue(4);
+		Assert::IsTrue(queue.empty());
+		Assert::AreEqual<size_t>(0, queue.size());
+	}
 
-	//TEST_METHOD(push_pop_one_thread)
-	//{
-	//	std::unique_ptr<int> p0 = std::make_unique<int>(24);
-	//	std::unique_ptr<int> p1 = std::make_unique<int>(100);
+	TEST_METHOD(push_pop_one_thread)
+	{
+		std::unique_ptr<int> p0 = std::make_unique<int>(24);
+		std::unique_ptr<int> p1 = std::make_unique<int>(100);
 
-	//	// push
-	//	concurrent_queue<std::unique_ptr<int>> queue(2);
-	//	queue.push(std::move(p0));
-	//	queue.push(std::move(p1));
+		// push
+		concurrent_queue<std::unique_ptr<int>> queue(2);
+		queue.push(std::move(p0));
+		queue.push(std::move(p1));
 
-	//	Assert::IsFalse(bool(p0));
-	//	Assert::IsFalse(bool(p1));
-	//	Assert::AreEqual<size_t>(2, queue.size());
+		Assert::IsFalse(bool(p0));
+		Assert::IsFalse(bool(p1));
+		Assert::AreEqual<size_t>(2, queue.size());
 
-	//	// pop
-	//	std::unique_ptr<int> out;
+		// pop
+		std::unique_ptr<int> out;
 
-	//	// pop 24 back
-	//	Assert::IsTrue(queue.try_pop(out));
-	//	Assert::AreEqual(24, *out);
-	//	Assert::AreEqual<size_t>(1, queue.size());
+		// pop 24 back
+		Assert::IsTrue(queue.try_pop(out));
+		Assert::AreEqual(24, *out);
+		Assert::AreEqual<size_t>(1, queue.size());
 
-	//	// pop 100 back
-	//	Assert::IsTrue(queue.wait_pop(out));
-	//	Assert::AreEqual(100, *out);
-	//	Assert::IsTrue(queue.empty());
-	//	Assert::AreEqual<size_t>(0, queue.size());
-	//}
+		// pop 100 back
+		Assert::IsTrue(queue.wait_pop(out));
+		Assert::AreEqual(100, *out);
+		Assert::IsTrue(queue.empty());
+		Assert::AreEqual<size_t>(0, queue.size());
+	}
 
-	//TEST_METHOD(push_pop_several_threads)
-	//{
-	//	const size_t thread_count = 29; // this_thread is not taken into account here
-	//	const size_t elements_per_thread = 3000;
-	//	using it_t = std::vector<int>::iterator;
+	TEST_METHOD(push_pop_several_threads)
+	{
+		const size_t thread_count = 29; // this_thread is not taken into account here
+		const size_t elements_per_thread = 3000;
+		using it_t = std::vector<int>::iterator;
 
-	//	std::vector<int> origin_vector((thread_count + 1) * elements_per_thread); // thread_count + 1 - 1 means this_thread
-	//	std::iota(origin_vector.begin(), origin_vector.end(), 0);
-	//
+		std::vector<int> origin_vector((thread_count + 1) * elements_per_thread); // thread_count + 1 - 1 means this_thread
+		std::iota(origin_vector.begin(), origin_vector.end(), 0);
+	
 
-	//	concurrent_queue<int> queue(origin_vector.size());
-	//	std::vector<int> actual_vector = origin_vector;
+		concurrent_queue<int> queue(origin_vector.size());
+		std::vector<int> actual_vector = origin_vector;
 
-	//	// worker_func acts as a producer and a consumer interchangeable.
-	//	// puts all the values from [begin, end) to the queue.
-	//	auto worker_func = [&queue, &actual_vector](it_t begin, it_t end) 
-	//	{
-	//		// put values into the queue
-	//		for (auto i = begin; i != end; ++i)
-	//			queue.push(*i);
+		// worker_func acts as a producer and a consumer interchangeable.
+		// puts all the values from [begin, end) to the queue.
+		auto worker_func = [&queue, &actual_vector](it_t begin, it_t end) 
+		{
+			// put values into the queue
+			for (auto i = begin; i != end; ++i)
+				queue.push(*i);
 
-	//		// try_pop
-	//		// get values from the queue and put them back into [begin, end)
-	//		for (auto i = begin; i != end; ++i) {
-	//			int v;
-	//			while (!queue.try_pop(v)) { ; }
-	//			
-	//			*i = v;
-	//		}
+			// try_pop
+			// get values from the queue and put them back into [begin, end)
+			for (auto i = begin; i != end; ++i) {
+				int v;
+				while (!queue.try_pop(v)) { ; }
+				
+				*i = v;
+			}
 
-	//		// wait_pop
-	//		// put values into the queue again
-	//		for (auto i = begin; i != end; ++i)
-	//			queue.push(*i);
+			// wait_pop
+			// put values into the queue again
+			for (auto i = begin; i != end; ++i)
+				queue.push(*i);
 
-	//		// get values from the queue and put them back into [begin, end)
-	//		for (auto i = begin; i != end; ++i) {
-	//			int v;
-	//			queue.wait_pop(v);
+			// get values from the queue and put them back into [begin, end)
+			for (auto i = begin; i != end; ++i) {
+				int v;
+				queue.wait_pop(v);
 
-	//			*i = v;
-	//		}
-	//	};
+				*i = v;
+			}
+		};
 
-	//	
-	//	// spawn worker threads
-	//	std::vector<std::thread> threads;
-	//	threads.reserve(thread_count);
+		
+		// spawn worker threads
+		std::vector<std::thread> threads;
+		threads.reserve(thread_count);
 
-	//	for (size_t i = 0; i < thread_count; ++i) {
-	//		it_t begin = actual_vector.begin() + i * elements_per_thread;
-	//		it_t end = begin + elements_per_thread;
-	//		threads.emplace_back(worker_func, begin, end);
-	//	}
+		for (size_t i = 0; i < thread_count; ++i) {
+			it_t begin = actual_vector.begin() + i * elements_per_thread;
+			it_t end = begin + elements_per_thread;
+			threads.emplace_back(worker_func, begin, end);
+		}
 
-	//	it_t begin = actual_vector.end() - elements_per_thread;
-	//	it_t end = actual_vector.end();
-	//	worker_func(begin, end);
+		it_t begin = actual_vector.end() - elements_per_thread;
+		it_t end = actual_vector.end();
+		worker_func(begin, end);
 
-	//	for (auto& t : threads)
-	//		t.join();
+		for (auto& t : threads)
+			t.join();
 
-	//	std::sort(actual_vector.begin(), actual_vector.end());
-	//	Assert::IsTrue(std::equal(origin_vector.cbegin(), origin_vector.cend(), actual_vector.cbegin()));
-	//}
+		std::sort(actual_vector.begin(), actual_vector.end());
+		Assert::IsTrue(std::equal(origin_vector.cbegin(), origin_vector.cend(), actual_vector.cbegin()));
+	}
 
-	//TEST_METHOD(push_wait_allowed)
-	//{
-	//	concurrent_queue<int> queue(1);
-	//	std::atomic_bool wait_flag(true);
+	TEST_METHOD(push_wait_allowed)
+	{
+		concurrent_queue<int> queue(1);
+		std::atomic_bool wait_flag(true);
 
-	//	std::thread waiter([&queue, &wait_flag] {
-	//		int v;
-	//		queue.wait_pop(v, wait_flag);
-	//	});
+		std::thread waiter([&queue, &wait_flag] {
+			int v;
+			queue.wait_pop(v, wait_flag);
+		});
 
-	//	wait_flag = false;
-	//	waiter.join(); // if wait_flag does not work, we are going to wait forever }:]
-	//}
+		wait_flag = false;
+		waiter.join(); // if wait_flag does not work, we are going to wait forever }:]
+	}
 };
 
 } // namespace
